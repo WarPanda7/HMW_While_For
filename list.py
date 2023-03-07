871030f406370783f8d944e03f50a724f3a6dd74
WarPanda7 <126077492+WarPanda7@users.noreply.github.com>
2023-03-07 19:07:09 +0200

    Update list.py

--- list.py
+++ list.py
@@ -14,7 +14,6 @@ for gi in range(2):
     print("Lesson| 1 | 2 | 3 | 4 | 5 |")
     for si in range(3):
         print("St:",si+1,end=" | ",)
-        
         for li in range(5):
             print(marks[gi][si][li],end=" | ")
         print()

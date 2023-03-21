9d697f4832c912345d85714b9e4c8e9dc5b775b5
WarPanda7 <126077492+WarPanda7@users.noreply.github.com>
2023-03-21 21:37:35 +0200

    Add files via upload

---
+++ def_lang.py
@@ -0,0 +1,8 @@
+def hi(lang, name):
+    if lang =="en":
+        print("Hello",name)
+    if lang =="ro":
+        print("Buna",name)
+    if lang =="ru":
+        print("Privet",name)
+hi("ru", "Vanea")
